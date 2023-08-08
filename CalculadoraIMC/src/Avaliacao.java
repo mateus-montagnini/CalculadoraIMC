be6ca4b4c0408b50c23747817e30aab0610a9b9a
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-08 16:41:23 -0300

    Iniciando projeto

---
+++ CalculadoraIMC/src/Avaliacao.java
@@ -0,0 +1,5 @@
+public class Avaliacao {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ CalculadoraIMC/src/Calculadora.java
@@ -0,0 +1,5 @@
+public class Calculadora {
+    public void calculadora() {
+        
+    }
+}
