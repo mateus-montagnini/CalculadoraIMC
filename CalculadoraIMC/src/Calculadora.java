97d6484b3a8717304b4e7a5caf40441f7468a922
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-08 16:47:32 -0300

    Criacao do metodo calculadora

--- CalculadoraIMC/src/Calculadora.java
+++ CalculadoraIMC/src/Calculadora.java
@@ -1,5 +1,7 @@
 public class Calculadora {
-    public void calculadora() {
+    public void calculadora(double peso, double altura) {
         
+        double imc = peso / (Math.pow(altura, 2));
+        System.out.println(imc);
     }
 }
